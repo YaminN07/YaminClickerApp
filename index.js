83855c7f8adbac1b45d5bea58007d2efa3f46d46
YaminN07 <144473898+YaminN07@users.noreply.github.com>
2024-10-15 10:59:40 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.getElementById("count-el").innerText = 5
+document.getElementById("count-el").innerText = 0
+
+// change title
+document.title = "students entered counter"
+
+let studentCount = 0
+function increment() {
+    studentCount ++;
+    document.getElementById("count-el").innerText = studentCount;
+}
